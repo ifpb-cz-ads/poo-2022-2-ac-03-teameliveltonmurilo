54a3bb65a25fe01ed7070b2c671dfb69a37dcf62
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-10-19 19:30:20 -0300

    Livro Batista & Moraes - Questão 5

---
+++ DiaDaSemana.java
@@ -0,0 +1,29 @@
+public class DiaDaSemana{
+    public static void main(String[] args){
+        int numeroDoDia = 5;
+
+        switch(numeroDoDia){
+            case 1:
+                System.out.println("O dia é Domingo");
+                break;
+            case 2:
+                System.out.println("O dia é Segunda");
+                break;
+            case 3:
+                System.out.println("O dia é Terça");
+                break;
+            case 4:
+                System.out.println("O dia é Quarta");
+                break;
+            case 5:
+                System.out.println("O dia é Quinta");
+                break;
+            case 6:
+                System.out.println("O dia é Sexta");
+                break;
+            case 7:
+                System.out.println("O dia é Sábado");
+                break;            
+        }
+    }
+}
